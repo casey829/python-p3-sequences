6a957ec022fc0a6ffe69cd0369e9d5d0e4459db3
casey <caseymwangi31@gmail.com>
2024-05-22 14:40:27 +0300

    understanding data structures

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if (length == 0):
+        print([])
+        return
+    elif (length == 1):
+        print([0])
+        return
+    elif (length == 2):
+        print([0,1])
+        return
+    else:
+        fib = [0, 1]
+        for i in range(2, length):
+            fib.append(fib[i-1] + fib[i-2])
+            if i == length - 1:
+                print(fib)
